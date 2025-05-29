ff34bde6b874df535deed2919a8c8ab45fa04640
Ramon Acitores <ramon.acitores@consensys.net>
2025-05-29 11:42:39 +0200

    fix: activity list check

--- test/e2e/stx-e2e/send-stx.spec.ts
+++ test/e2e/stx-e2e/send-stx.spec.ts
@@ -7,6 +7,7 @@ import FixtureBuilder from '../fixture-builder';
 import HeaderNavbar from '../page-objects/pages/header-navbar';
 import AccountListPage from '../page-objects/pages/account-list-page';
 import dotenv from 'dotenv';
+import ActivityListPage from '../page-objects/pages/home/activity-list';
 
 dotenv.config();
 
@@ -20,7 +21,7 @@ describe('Send STX', function () {
             selectedNetworkClientId: 'bnb-mainnet',
           })
           .build(),
-        title: this.test.fullTitle(),
+        title: this.test?.fullTitle(),
         localNodeOptions: 'none',
         dapp: false,
         disableMocking: true,
@@ -39,7 +40,7 @@ describe('Send STX', function () {
 
         await accountListPage.openAccountOptionsMenu();
         await accountListPage.addNewImportedAccount(
-          process.env.TEST_STX_PRIVATE_KEY,
+          process.env.TEST_STX_PRIVATE_KEY || '',
         );
 
         // wait to have the balance loaded when send
@@ -87,56 +88,9 @@ describe('Send STX', function () {
           '[data-testid="account-overview__activity-tab"]',
         );
 
-        // await driver.wait(async () => {
-        //   const confirmedTxes = await driver.findElements(
-        //     '.transaction-list__completed-transactions .activity-list-item',
-        //   );
-        //   if (confirmedTxes.length >= 2) {
-        //     try {
-        //       const firstTxElement = await confirmedTxes[0].findElement(
-        //         '[data-testid="activity-list-item-action"]',
-        //       );
-        //       const secondTxElement = await confirmedTxes[1].findElement(
-        //         '[data-testid="activity-list-item-action"]',
-        //       );
-        //       const firstTxType = await firstTxElement.getText();
-        //       const secondTxType = await secondTxElement.getText();
-        //       return firstTxType === 'Receive' && secondTxType === 'Send';
-        //     } catch (e) {
-        //       return false;
-        //     }
-        //   }
-        //   return false;
-        // }, 10000);
-
-        await driver.waitForSelector({
-          css: '[data-testid="transaction-list-item-primary-currency"]',
-          text: '-0.00001 BNB',
-        });
-
-        const allTX = await driver.findElements(
-          '[data-testid="activity-list-item"]',
-        );
-        console.log(allTX.length);
-        const sentTX = await allTX.find((tx) =>
-          tx.findElement({
-            css: '[data-testid="activity-list-item-action"]',
-            text: 'Send',
-          }),
-        );
-        console.log(sentTX);
-        await driver.wait(async () => {
-          const completedTx = await sentTX.findElement(
-            '.transaction-list__completed-transactions .activity-list-item',
-          );
-          return completedTx ? true : false;
-        }, 10000);
-
-        // assert.equal(
-        //   (await driver.findElements('.transaction-status-label--confirmed'))
-        //     .length,
-        //   1,
-        // );
+        const activityListPage = new ActivityListPage(driver);
+        await activityListPage.check_txAction('Send');
+        await activityListPage.check_txStatusConfirmed();
       },
     );
   });
